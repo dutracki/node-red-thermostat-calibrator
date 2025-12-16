2c8f8b09d0706d6e8f81adde497d0d589e7e7408
dutracki <dariusz.utracki@digitaly.site>
2025-12-16 21:38:14 +0100

    fix: trigger calc on BOTH sensor AND thermostat updates

--- function.js
+++ function.js
@@ -92,8 +92,6 @@ const loc = dev.loc;
 const state = getState(loc);
 
 // --- INGESTION ---
-let isSensorUpdate = false;
-
 if (dev.type === 'sensor') {
     const temp = msg.payload?.temperature;
     if (typeof temp !== 'number') return null;
@@ -101,7 +99,6 @@ if (dev.type === 'sensor') {
     state.sensors[topic] = { temp, ts: now, weight: dev.weight };
     setState(loc, state);
     log(`[${loc}] Sensor ${topic.split('/').pop()}: ${temp}°C`);
-    isSensorUpdate = true;
 
 } else if (dev.type === 'thermostat') {
     const temp = msg.payload?.local_temperature;
@@ -110,15 +107,11 @@ if (dev.type === 'sensor') {
 
     state.thermostat = { topic, temp, cal, ts: now };
     setState(loc, state);
-    log(`[${loc}] Thermo stored: cal=${cal}`);
-    // Thermostat update only stores state, does NOT trigger calculation
-    return null;
+    log(`[${loc}] Thermo: cal=${cal}`);
 }
 
-// --- CALCULATION (only on sensor update) ---
-if (!isSensorUpdate) return null;
-
-// Need thermostat data to calculate
+// --- CALCULATION (on both sensor AND thermostat update) ---
+// Need thermostat data
 if (!state.thermostat) {
     log(`[${loc}] Waiting for thermostat`);
     return null;
